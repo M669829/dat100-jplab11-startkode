f99d264a068ba8011951a6d0edc5226d375bc2d7
M669829 <669829@stud.hvl.no>
2022-11-15 10:11:09 +0100

    Add files via upload

--- src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
+++ src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
@@ -4,68 +4,84 @@ import no.hvl.dat100.jplab11.common.TODO;
 
 public abstract class Innlegg {
 	
-	// TODO - deklarering av objektvariable
+    int Id;
+	String Bruker;
+	String Dato;
+	int Likes;
 	
 	public Innlegg() {
-		
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+       Id =id;
+       Bruker =bruker;
+       Dato = dato;
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
 
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		Id = id;
+        Bruker = bruker;
+		Dato = dato;
+		Likes = likes;
 	}
 	
 	public String getBruker() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return Bruker; 
 
 	}
 
 	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		this.Bruker = bruker; 
 	}
 
 	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
+		return Dato;
 		
 	}
 
 	public void setDato(String dato) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.Dato = dato; 
 	}
 
 	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.Id;
 
 	}
 
 	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		return this.Likes;
 
 	}
 	
 	public void doLike () {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		Likes++;
 	}
 	
 	public boolean erLik(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		boolean lik;
+		
+		if (innlegg.Id == getId()) {
+			lik = true;
+		}else {
+			lik = false;
+		}
+return lik;
 	}
 	
 	@Override
 	public String toString() {
 		
-		throw new UnsupportedOperationException(TODO.method());
-				
+		String std;
+		
+		std = getId() +"\n"+ getBruker()+"\n"+getDato()+"\n"+getLikes()+"\n";
+		
+		return std;
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
