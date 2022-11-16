233cb55af545102ad5336e27fc3f292b95897691
vegar <vegar@Vegards-Surface.lan>
2022-11-16 20:38:29 +0100

    SIste oppgave

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,32 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.FileWriter;
+
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
-
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+			
+		int lengde = samling.getAntall();
+		
+		try {
+			FileWriter skriver = new FileWriter(mappe + filnavn);
+			
+			for(int i = 0; i<lengde;i++) {
+				skriver.write(samling.toString());
+			}
+			skriver.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+		
 	}
 }
